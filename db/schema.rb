2329c48d164db30c05418c9638144fb2b5dd43b1
Aaron K <kearlyaa@gmail.com>
2016-05-06 19:27:32 +0000

    Add feature - instructors can now add lessons

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506184117) do
+ActiveRecord::Schema.define(version: 20160506191233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 20160506184117) do
 
   add_index "installs", ["email"], name: "index_installs_on_email", unique: true, using: :btree
   add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true, using: :btree
+
+  create_table "lessons", force: true do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.integer  "section_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "lessons", ["section_id"], name: "index_lessons_on_section_id", using: :btree
 
   create_table "sections", force: true do |t|
     t.string   "title"
