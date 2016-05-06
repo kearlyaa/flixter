3a9e2e1b4941e1dd5b5c189a3a13a2762795adb4
Aaron K <kearlyaa@gmail.com>
2016-05-06 21:03:20 +0000

    Fixed typo in lessons controller naming, added lesson show view page.

---
+++ test/controllers/lesssons_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LesssonsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/lesssons_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class LesssonsHelperTest < ActionView::TestCase
+end
