3c943f3f6e15702e934d54adc89f60388cbaa3a2
Aaron K <kearlyaa@gmail.com>
2016-05-09 19:00:10 +0000

    Only allow users enrolled in a course to see associated lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,22 @@
 class LessonsController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :user_enrolled_in_course
+
   def show
   end
 
   private
+  
   helper_method :current_lesson
+  
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def user_enrolled_in_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'Please enroll in this course to view lessons'
+    end
   end
 end
 
